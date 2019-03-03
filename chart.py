708e462fe037c0a640c61fa52c50e8102b1a6d21
gmdmgithub <gmika@interia.pl>
2019-03-03 21:58:31 +0100

    bokeh, finishing the picture

--- chart.py
+++ chart.py
@@ -1,4 +1,8 @@
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Blues8
+from bokeh.embed import components
 import pandas
 
 def bokeh_print():
@@ -25,33 +29,82 @@ def data_plot():
     # Read in csv
     data_frame = pandas.read_csv('sample.csv')
 
-    car = data_frame['Car']
-    horse_power = data_frame['Horsepower']
+    # car = data_frame['Car']
+    # horse_power = data_frame['Horsepower']
+
+    # Create ColumnDataSource from data frame
+    source = ColumnDataSource(data_frame)
+
     output_file('index.html')
+    # Car list
+    car_list = source.data['Car'].tolist()
 
     # create a new plot with a title and axis labels
     fig = figure(
-        y_range=car,
-        plot_width=800,
-        plot_height=600,
+    #    y_range=car, # using 
+        y_range=car_list,
+        plot_width=700,
+        plot_height=400,
         title="Cars with top horsepower",
-        x_axis_label='horpower',
-        tools="pan,box_select,zoom_in,zoom_out,save,reset")
+        x_axis_label='Horsepower',
+        y_axis_label='Cars',
+        tools="pan,box_select,zoom_in,zoom_out,save,reset",
+        toolbar_location=None)
 
     #ranger graph
     # https://bokeh.pydata.org/en/latest/docs/reference/models/glyphs/hbar.html
     fig.hbar(
-        y=car,
-        right=horse_power,
+        # y=car,
+        # right=horse_power,
+        y='Car',
+        right='Horsepower',
         left=0,
         height=0.5,
-        color="blue",
-        fill_alpha=0.5
+        #color="#ff1200",
+        fill_color=factor_cmap(
+            'Car',
+            palette=Blues8,
+            factors=car_list
+        ),
+        fill_alpha=0.8,
+        source=source,
+        legend='Car'
 
     )
+    fig.toolbar_location = 'above'
+
+    # Add Legend
+    fig.legend.orientation = 'vertical'
+    fig.legend.location = 'top_right'
+    # print(fig.legend.__dir__())
+    fig.legend.label_text_font_size = '10px'
+
+    # Add Tooltips
+    hover = HoverTool()
+    hover.tooltips = """
+    <div>
+        <h3>@Car</h3>
+        <div><strong>Price: </strong>@Price</div>
+        <div><strong>HP: </strong>@Horsepower</div>
+        <div><img src="@Image" alt="" width="200" /></div>
+    </div>
+    """
+    fig.add_tools(hover)
 
      # show the results
-    show(fig)
+    # show(fig) #like f5
+    save(fig)
+
+    # Use compoenten from bokeh.embed to prepare script and div
+    ## IMPORTANT - use it then in respond!!
+    script, div = components(fig)
+    # print(div)
+    # print(script)
+    f= open("fig.div","w")
+    f.write(div)
+    f.write(script)
+    f.close()
+
 if __name__ == "__main__":
     # bokeh_print()
     data_plot()
